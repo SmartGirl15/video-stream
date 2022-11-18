e0fc5ec1843f8dda6a91c9d8fe574dcd8a461bc4
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2022-11-18 13:37:01 +0530

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -12,6 +12,7 @@ from urllib.parse import urlparse
 import aiofiles
 import aiohttp
 import requests
+import wget
 import yt_dlp
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, MessageNotModified
